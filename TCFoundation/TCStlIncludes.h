7871b80749a56f9c6cbe03a38ad452f81b2b4e26
Travis Cobbs <tcobbs@example.com>
2006-10-22 06:41:17 +0000

    Created one include file for STL includes.  They require messed up pragmas in Windows to prevent huge numbers of warnings, and instead of having to do that in every source file that uses them, I'll just put it in one place.

---
+++ TCFoundation/TCStlIncludes.h
@@ -0,0 +1,24 @@
+#ifndef __TCSTLINCLUDES_H__
+#define __TCSTLINCLUDES_H__
+
+#ifdef WIN32
+// In Windows, we have to disable a number of warnings in order to use any STL
+// classes without getting tons of warnings.  The following warning is shut off
+// completely; it's just the warning that identifiers longer than 255 characters
+// will be truncated in the debug info.  I really don't care about this.  Note
+// that the other warnings are only disabled during the #include of the STL
+// headers due to the warning(push) and warning(pop).
+#pragma warning(push, 1)	// Minimum warnings during STL includes
+// Disable more warnings.  Note that they're reenable at the pop below.
+// 4702: Unreachable code.
+// 4786: Identifier truncated to 255 characters in debug info.
+#pragma warning(disable: 4702 4786)	// Unreachable code warnings pop up also.
+#endif // WIN32
+#include <string>
+#include <map>
+#include <vector>
+#ifdef WIN32
+#pragma warning(pop)
+#endif // WIN32
+
+#endif __TCSTLINCLUDES_H__
