c4a04626ff39685358637416c2c8b26480eb5047
Travis Cobbs <tcobbs@gmail.com>
2006-12-02 08:23:24 +0000

    Update to make view menu have popup arrow attached to the right of it.

--- QT/toolbarimages.sh
+++ QT/toolbarimages.sh
@@ -2,7 +2,15 @@
 rm images/toolbar_*.png
 X=0
 for i in reload wireframe seams edge privsubs snapshot view light preferences bfc ; do
-bmptoppm ../Icons/toolbar.bmp | pnmcut -left $X -top 0 -width 16 -height 16 |\
-pnmtopng -transparent rgb:c0/c0/c0 >images/toolbar_$i.png
+Popup=0
+case $i in ( view ) Popup=1;; esac
+if [ $Popup -eq 1 ]; then
+	bmptoppm ../Icons/toolbar.bmp | pnmcut -left $X -top 0 -width 16 -height 16\
+	| pnmcat -lr - images/toolbar_popup_arrow.ppm | pnmtopng -transparent\
+	rgb:c0/c0/c0 >images/toolbar_$i.png
+else
+	bmptoppm ../Icons/toolbar.bmp | pnmcut -left $X -top 0 -width 16 -height 16\
+	| pnmtopng -transparent rgb:c0/c0/c0 >images/toolbar_$i.png
+fi
 X=`expr $X + 16`
 done
