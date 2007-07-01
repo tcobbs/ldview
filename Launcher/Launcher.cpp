fb42262bf27e2115db84a5a8e15e15852f11aa31
Travis Cobbs <tcobbs@gmail.com>
2007-07-01 21:22:41 +0000

    Update to not require AttachConsole, and add VC6 support.

--- Launcher/Launcher.cpp
+++ Launcher/Launcher.cpp
@@ -17,6 +17,13 @@
 #include <shlwapi.h>
 #include <stdio.h>
 
+#if _MSC_VER < 1400	// VC < VC 2005
+#define _tcscpy_s(dst, bufSize, src) _tcscpy(dst, src)
+#define _tcscat_s(dst, bufSize, src) _tcscat(dst, src)
+#define _tcsncpy_s(dst, bufSize, src, len) _tcsncpy(dst, src, len)
+#define __w64
+#endif // VC < VC 2005
+
 int launchExe(_TCHAR *appName, _TCHAR *exeFilename, int argc, _TCHAR* argv[])
 {
 	int i;
@@ -26,7 +33,28 @@ int launchExe(_TCHAR *appName, _TCHAR *exeFilename, int argc, _TCHAR* argv[])
 	PROCESS_INFORMATION processInfo;
 	DWORD priority = NORMAL_PRIORITY_CLASS;
 	_TCHAR title[1024];
+	SECURITY_ATTRIBUTES sa;
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE hChildStdOutRd;
+	HANDLE hChildStdOutRdDup;
+	HANDLE hChildStdOutWr;
 
+	memset(&sa, 0, sizeof(sa));
+	sa.nLength = sizeof(sa);
+	sa.bInheritHandle = TRUE;
+	if (!CreatePipe(&hChildStdOutRd, &hChildStdOutWr, &sa, 0))
+	{
+		_tprintf(_T("Error creating pipe.\n"));
+		return 1;
+	}
+	if (!DuplicateHandle(GetCurrentProcess(), hChildStdOutRd,
+		GetCurrentProcess(), &hChildStdOutRdDup, 0, FALSE,
+		DUPLICATE_SAME_ACCESS))
+	{
+		_tprintf(_T("Error restricting pipe.\n"));
+		return 1;
+	}
+	CloseHandle(hChildStdOutRd);
 	for (i = 1; i < argc; i++)
 	{
 		len += _tcslen(argv[i]) + 1;
@@ -41,29 +69,45 @@ int launchExe(_TCHAR *appName, _TCHAR *exeFilename, int argc, _TCHAR* argv[])
 	}
 	memset(&startupInfo, 0, sizeof(startupInfo));
 	startupInfo.cb = sizeof(startupInfo);
+#if _MSC_VER < 1400	// VC < VC 2005
+	_stprintf(title, _T("Command Line %s"), appName);
+#else // VC < VC 2005
 	_stprintf_s(title, sizeof(title) / sizeof(title[0]), _T("Command Line %s"),
 		appName);
+#endif // VC < VC 2005
 	startupInfo.lpTitle = title;
 	startupInfo.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
-	startupInfo.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	startupInfo.hStdOutput = hChildStdOutWr;
 	startupInfo.hStdError = GetStdHandle(STD_ERROR_HANDLE);
-	startupInfo.dwFlags = STARTF_USESTDHANDLES | STARTF_USEFILLATTRIBUTE;
-	startupInfo.dwFillAttribute = FOREGROUND_RED | BACKGROUND_RED |
-		BACKGROUND_GREEN | BACKGROUND_BLUE;
-	if (CreateProcess(exeFilename, commandLine, NULL, NULL, FALSE,
+	startupInfo.dwFlags = STARTF_USESTDHANDLES;
+	if (CreateProcess(exeFilename, commandLine, NULL, NULL, TRUE,
 		priority, NULL, NULL, &startupInfo, &processInfo))
 	{
+		CloseHandle(processInfo.hThread);
+		CloseHandle(hChildStdOutWr);
 		// while (true) now generates a warning.
 		for ( ; ; )
 		{
+			CHAR chBuf[4096];
+			DWORD dwRead = 0;
+			DWORD dwWritten = 0;
 			DWORD exitCode;
 
+			if (ReadFile(hChildStdOutRdDup, chBuf, sizeof(chBuf), &dwRead, NULL)
+				&& dwRead > 0)
+			{
+				WriteConsole(hStdOut, chBuf, dwRead / sizeof(TCHAR), &dwWritten, NULL);
+			}
+			else
+			{
+				Sleep(50);
+			}
 			GetExitCodeProcess(processInfo.hProcess, &exitCode);
 			if (exitCode != STILL_ACTIVE)
 			{
+				CloseHandle(processInfo.hProcess);
 				return 0;
 			}
-			Sleep(50);
 		}
 	}
 	else
@@ -84,8 +128,13 @@ int launchExe(_TCHAR *appName, _TCHAR *exeFilename, int argc, _TCHAR* argv[])
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	_TCHAR *dotSpot = _tcsrchr(argv[0], '.');
+	size_t appLen = _tcslen(argv[0]);
+	_TCHAR *dotSpot = &argv[0][appLen - 4];
 
+	if (dotSpot[0] != '.')
+	{
+		dotSpot = NULL;
+	}
 	if (!dotSpot)
 	{
 		dotSpot = &argv[0][_tcslen(argv[0])];
@@ -94,7 +143,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	__w64 int fullLen = len + 5;
 	_TCHAR *appName = new _TCHAR[len + 1];
 	_TCHAR *exeFilename = new _TCHAR[fullLen];
-
 	_tcsncpy_s(appName, len + 1, argv[0], len);
 	appName[len] = 0;
 	_tcsncpy_s(exeFilename, fullLen, argv[0], len);
