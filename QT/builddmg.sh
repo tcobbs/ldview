1281af9e4fc73e45c53e086f6211289e2375a5c7
Travis Cobbs <tcobbs@example.com>
2007-07-14 21:53:03 +0000

    Add script for creating Mac release package.

---
+++ QT/builddmg.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+strip LDView.app/Contents/MacOS/LDView
+if [ ! -d distrib ]; then
+	mkdir distrib
+fi
+if [ -d distrib/LDView ]; then
+	rm -rf distrib/LDView
+fi
+mkdir distrib/LDView
+FileVersion=`grep 'VALUE "FileVersion"' ../LDView.rc | cut -d, -f2 | cut -d\" -f2 | cut -d\\\\ -f1`
+Filename="LDView $FileVersion Universal"
+cp ../8464.mpd distrib/LDView/
+cp ../ChangeHistory.html distrib/LDView
+cp -R LDView.App distrib/LDView/
+cp ../license.txt distrib/LDView
+cp ../m6459.ldr distrib/LDView
+hdiutil create -ov -srcfolder distrib/LDView -volname "$Filename" -format UDBZ "distrib/$Filename.dmg"
