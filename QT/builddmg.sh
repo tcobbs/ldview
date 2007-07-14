eb218b67f2e74f524ba84a39ae5f27fca5016143
Travis Cobbs <tcobbs@example.com>
2007-07-14 22:03:43 +0000

    Mac cleanup.

--- QT/builddmg.sh
+++ QT/builddmg.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+echo Stripping executable...
 strip LDView.app/Contents/MacOS/LDView
 if [ ! -d distrib ]; then
 	mkdir distrib
@@ -10,9 +11,11 @@ fi
 mkdir distrib/LDView
 FileVersion=`grep 'VALUE "FileVersion"' ../LDView.rc | cut -d, -f2 | cut -d\" -f2 | cut -d\\\\ -f1`
 Filename="LDView $FileVersion Universal"
+echo Copying files...
 cp ../8464.mpd distrib/LDView/
 cp ../ChangeHistory.html distrib/LDView
 cp -R LDView.App distrib/LDView/
 cp ../license.txt distrib/LDView
 cp ../m6459.ldr distrib/LDView
+echo Building DMG...
 hdiutil create -ov -srcfolder distrib/LDView -volname "$Filename" -format UDBZ "distrib/$Filename.dmg"
---
+++ QT/macrun.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+/Users/travis/tmp/LDView/QT/LDView.app/Contents/MacOS/LDView $*
