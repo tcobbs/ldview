c6206a6e6d8cb2b7eb567bc0938c38a19d0b8dac
Travis Cobbs <tcobbs@example.com>
2007-08-30 05:38:16 +0000

    Remove platform-specific code; it'll go in the callers.

--- LDLib/LDInputHandler.h
+++ LDLib/LDInputHandler.h
@@ -36,7 +36,7 @@ public:
 	bool mouseWheel(TCULong modifierKeys, TCFloat amount);
 	void setMouseUpPending(bool value);
 
-	static TCULong convertModifiers(TCULong osModifiers);
+	//static TCULong convertModifiers(TCULong osModifiers);
 	static const char *captureAlertClass(void) { return "LDCaptureMouse"; }
 	static const char *releaseAlertClass(void) { return "LDReleaseMouse"; }
 	static const char *peekMouseUpAlertClass(void) { return "LDPeekMouseUp"; }
