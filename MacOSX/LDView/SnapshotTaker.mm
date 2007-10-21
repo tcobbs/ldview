c7ad946f09fe86481618ef157a3dcaf12df8fb1c
Travis Cobbs <tcobbs@gmail.com>
2007-10-21 23:29:48 +0000

    Initial work on Cocoa command line snapshots.

--- MacOSX/LDView/SnapshotTaker.mm
+++ MacOSX/LDView/SnapshotTaker.mm
@@ -8,7 +8,6 @@
 
 #import "SnapshotTaker.h"
 #include <LDLib/LDrawModelViewer.h>
-#include <TRE/TREGL.h>
 
 #define PB_WIDTH 1024
 #define PB_HEIGHT 1024
@@ -20,19 +19,30 @@
 	return [self initWithModelViewer:aModelViewer sharedContext:nil];
 }
 
-- (NSOpenGLPixelFormat *)pixelFormat
+- (BOOL)choosePixelFormat:(CGLPixelFormatObj *)pPixelFormat sharedContext:(NSOpenGLContext *)sharedContext;
 {
-	NSOpenGLPixelFormatAttribute attrs[] =
+	CGLPixelFormatAttribute attrs[] =
 	{
-		NSOpenGLPFADepthSize, (NSOpenGLPixelFormatAttribute)24,
-		NSOpenGLPFAColorSize, (NSOpenGLPixelFormatAttribute)24,
-		NSOpenGLPFAAlphaSize, (NSOpenGLPixelFormatAttribute)8,
-		NSOpenGLPFAStencilSize, (NSOpenGLPixelFormatAttribute)8,
-		NSOpenGLPFAAccelerated, (NSOpenGLPixelFormatAttribute)NO,
-		NSOpenGLPFAPixelBuffer,
-		(NSOpenGLPixelFormatAttribute)0
+		kCGLPFADepthSize, (CGLPixelFormatAttribute)24,
+		kCGLPFAColorSize, (CGLPixelFormatAttribute)24,
+		kCGLPFAAlphaSize, (CGLPixelFormatAttribute)8,
+		kCGLPFAStencilSize, (CGLPixelFormatAttribute)8,
+		kCGLPFAAccelerated, (CGLPixelFormatAttribute)NO,
+		kCGLPFAPBuffer,
+		(CGLPixelFormatAttribute)0,
+		(CGLPixelFormatAttribute)0
 	};
-	return [[[NSOpenGLPixelFormat alloc] initWithAttributes:attrs] autorelease];
+	long num;
+
+	if (sharedContext == nil)
+	{
+		attrs[sizeof(attrs) / sizeof(attrs[0]) - 2] = kCGLPFARemotePBuffer;
+	}
+	if (CGLChoosePixelFormat(attrs, pPixelFormat, &num) == kCGLNoError)
+	{
+		return YES;
+	}
+	return NO;
 }
 
 - (id)initWithModelViewer:(LDrawModelViewer *)aModelViewer sharedContext:(NSOpenGLContext *)sharedContext
@@ -40,18 +50,29 @@
 	self = [super init];
 	if (self)
 	{
-		NSOpenGLPixelBuffer *pixelBuffer = [[NSOpenGLPixelBuffer alloc] initWithTextureTarget:GL_TEXTURE_2D textureInternalFormat:GL_RGBA textureMaxMipMapLevel:0 pixelsWide:PB_WIDTH pixelsHigh:PB_HEIGHT];
-
 		modelViewer = aModelViewer;
 		ldSnapshotTaker = new LDSnapshotTaker(modelViewer);
-		glContext = [[NSOpenGLContext alloc] initWithFormat:[self pixelFormat] shareContext:sharedContext];
-		[glContext setPixelBuffer:pixelBuffer cubeMapFace:0 mipMapLevel:0 currentVirtualScreen:[glContext currentVirtualScreen]];
-		[pixelBuffer release];
-		if (!glContext)
-		{
-			[self dealloc];
-			return nil;
+		if (CGLCreatePBuffer(PB_WIDTH, PB_HEIGHT, GL_TEXTURE_2D, GL_RGBA, 0, &pbuffer) == kCGLNoError)
+		{			
+			CGLPixelFormatObj pixelFormat;
+
+			if ([self choosePixelFormat:&pixelFormat sharedContext:sharedContext])
+			{
+				if (CGLCreateContext(pixelFormat, (CGLContextObj)[sharedContext CGLContextObj], &context) == kCGLNoError)
+				{
+					long virtualScreen;
+
+					CGLDestroyPixelFormat(pixelFormat);
+					CGLSetCurrentContext(context);
+					CGLGetVirtualScreen(context, &virtualScreen);
+					CGLSetPBuffer(context, pbuffer, 0, 0, virtualScreen);
+					return self;
+				}
+				CGLDestroyPixelFormat(pixelFormat);
+			}
 		}
+		[self dealloc];
+		return nil;
 	}
 	return self;
 }
@@ -59,7 +80,14 @@
 - (void)dealloc
 {
 	TCObject::release(ldSnapshotTaker);
-	[glContext release];
+	if (context)
+	{
+		CGLDestroyContext(context);
+	}
+	if (pbuffer)
+	{
+		CGLDestroyPBuffer(pbuffer);
+	}
 	[super dealloc];
 }
 
@@ -77,12 +105,16 @@
 {
 	bool retValue;
 
-	[glContext makeCurrentContext];
+	CGLSetCurrentContext(context);
 	glViewport(0, 0, PB_WIDTH, PB_HEIGHT);
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_DEPTH_TEST);
 	glDrawBuffer(GL_FRONT);
 	glReadBuffer(GL_FRONT);
+	if (modelViewer->getMainTREModel() == NULL && !modelViewer->getNeedsReload())
+	{
+		modelViewer->loadModel(true);
+	}
 	modelViewer->setup();
 	retValue = ldSnapshotTaker->saveImage([filename cStringUsingEncoding:NSASCIIStringEncoding], width, height, zoomToFit);
 	return retValue;
