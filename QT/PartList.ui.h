c6197cb276a1b5ab1691089c9b76967982c18aa7
Peter Bartfai <pbartfai@stardust.hu>
2008-04-20 20:02:40 +0000

    Migrate PartListPanel to subclassing approach. Preparation for Qt4 migration

--- QT/PartList.ui.h
+++ QT/PartList.ui.h
@@ -1,35 +1,20 @@
-void PartListPanel::setPartList( PartList *value)
-{
-	partlist = value;
-}
-
-void PartListPanel::init()
-{
-}
-
 void PartListPanel::doOk()
 {
-	partlist->doOk();
-	accept();
 }
 
 void PartListPanel::doCancel()
 {
-	reject();
 }
 
 void PartListPanel::doUp()
 {
-	partlist->doUp();
 }
 
 void PartListPanel::doDown()
 {
-	partlist->doDown();
 }
 
 void PartListPanel::doHighlighted()
 {
-	partlist->controlDirectionButtons();
 }
 
