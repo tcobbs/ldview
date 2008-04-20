8bc54ae55bded92e747c19fcbcbe8ab3432561ca
Peter Bartfai <pbartfai@stardust.hu>
2008-04-20 20:23:05 +0000

    Migrate SnapshotSettingsPanel to subclassing approach. Preparation for Qt4 migration

--- QT/LDViewSnapshotSettings.h
+++ QT/LDViewSnapshotSettings.h
@@ -2,28 +2,30 @@
 #define __LDVIEWSNAPSHOTSETTINGS_H__
 
 #include <TCFoundation/TCTypedObjectArray.h>
-#include "ModelViewerWidget.h"
+//#include "ModelViewerWidget.h"
+#include "SnapshotSettingsPanel.h"
 
-class SnapshotSettingsPanel;
 class QButton;
 
-class SnapshotSettings
+class SnapshotSettings : public SnapshotSettingsPanel
 {
 public:
 	SnapshotSettings(ModelViewerWidget *modelWidget);
 	~SnapshotSettings(void);
 
 	void reflectSettings();
-	void show(void);
+    void setButtonState(QCheckBox *button, bool state);
+	void doEnabledSeries();
+	void doEnabledSize();
+//	void show(void);
+public slots:
 	void clear(void);
 	void doOk(void);
 	void doCancel(void);
-	void setButtonState(QCheckBox *button, bool state);
 protected:
 
 	LDrawModelViewer *modelViewer;
 	ModelViewerWidget *modelWidget;
-	SnapshotSettingsPanel *panel;
 };
 
 #endif
