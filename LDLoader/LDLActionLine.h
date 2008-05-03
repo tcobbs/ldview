bc4499d9ac44f5f9492ba535917f4d2c2add7cb5
Travis Cobbs <tcobbs@gmail.com>
2008-05-03 23:35:48 +0000

    Fix OBI geometry in transparent parts.

--- LDLoader/LDLActionLine.h
+++ LDLoader/LDLActionLine.h
@@ -24,33 +24,6 @@ public:
 	virtual void scanPoints(TCObject *scanner,
 		LDLScanPointCallback scanPointCallback, const TCFloat *matrix,
 		LDLModel::ScanPointType types) const = 0;
-	// OBI
-	bool getObiOverrideColor(void) const
-	{
-		return m_actionFlags.obiOverrideColor != false;
-	}
-	void setObiOverrideColor(bool value)
-	{
-		m_actionFlags.obiOverrideColor = value;
-	}
-	int getObiOverrideColorNumber(void) const
-	{
-		return m_obiOverrideColorNumber;
-	}
-	void setObiOverrideColorNumber(int value)
-	{
-		setObiOverrideColor(true);
-		m_obiOverrideColorNumber = value;
-	}
-	bool getObiOverrideActive(void) const
-	{
-		return m_actionFlags.obiOverrideActive != false;
-	}
-	void setObiOverrideActive(bool value)
-	{
-		m_actionFlags.obiOverrideActive = value;
-	}
-	// /OBI
 protected:
 	LDLActionLine(LDLModel *parentModel, const char *line, int lineNumber,
 		const char *originalLine = NULL);
@@ -63,16 +36,8 @@ protected:
 		bool bfcClip:1;
 		bool bfcWindingCCW:1;
 		bool bfcInvert:1;
-		bool obiOverrideColor;		// in OBI mode, whether color is currently
-									// getting overridden
-		// Temporal flags
-		bool obiOverrideActive;		// Whether or not to return the override
-									// color or the original color
 	} m_actionFlags;
 	int m_colorNumber;
-
-	int m_obiOverrideColorNumber;	// in OBI mode, the color used to override
-									// either color "16" or "24"
 };
 
 #endif // __LDLACTIONLINE_H__
