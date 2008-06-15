3ffd32fec36824459864b005569d7b6e77e741c7
Travis Cobbs <tcobbs@example.com>
2008-06-15 04:49:25 +0000

    Created new class to handle save panel accessory views that contain file type selection.

---
+++ MacOSX/LDView/FileTypeViewOwner.h
@@ -0,0 +1,42 @@
+//
+//  FileTypeViewOwner.h
+//  LDView
+//
+//  Created by Travis Cobbs on 10/27/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ViewOwnerBase.h"
+#include <LDLib/LDSnapshotTaker.h>
+
+@interface FileTypeViewOwner : ViewOwnerBase
+{
+	IBOutlet NSView *accessoryView;
+	IBOutlet NSButton *fileTypeOptionsButton;
+	IBOutlet NSPopUpButton *fileTypePopUp;
+
+	NSTextField *nameFieldLabel;
+	NSTextField *filenameField;
+
+	NSSavePanel *savePanel;
+
+	NSMutableArray *fileTypes;
+	NSMutableArray *extensions;
+	int firstType;
+	int lastType;
+	NSString *udTypeKey;
+}
+
+- (id)init;
+- (void)setSavePanel:(NSSavePanel *)aSavePanel;
+- (void)saveSettings;
+
+- (IBAction)fileType:(id)sender;
+- (IBAction)fileTypeOptions:(id)sender;
+
+- (NSString *)requiredFileType;
+
+- (BOOL)haveOptions;
+
+@end
