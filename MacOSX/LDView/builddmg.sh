9a67a1e376edb1a4db01fba28b9ec2ab31935ca2
Travis Cobbs <tcobbs@gmail.com>
2009-01-05 05:31:22 +0000

    Added script to build release DMG.

---
+++ MacOSX/LDView/builddmg.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+echo Stripping executable...
+if [ ! -d distrib ]; then
+	mkdir distrib
+fi
+if [ -d distrib/LDView ]; then
+	rm -rf distrib/LDView
+fi
+mkdir distrib/LDView
+FileVersion=`grep 'VALUE "FileVersion"' ../../LDView.rc | cut -d, -f2 | cut -d\" -f2 | cut -d\\\\ -f1`
+Filename="LDView_${FileVersion}_Universal"
+echo Copying files...
+cp ../../8464.mpd distrib/LDView/
+cp ../../ChangeHistory.html distrib/LDView
+cp -R build/Release_Universal/LDView.app distrib/LDView/
+cp ../../license.txt distrib/LDView
+cp ../../m6459.ldr distrib/LDView
+echo Building DMG...
+hdiutil create -ov -srcfolder distrib/LDView -volname "$Filename" -format UDBZ "distrib/$Filename.dmg"
