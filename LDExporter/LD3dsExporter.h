bc6bf7faf8abd0cae3d1f4b451be3b08b8ba7545
Travis Cobbs <tcobbs@example.com>
2009-03-22 21:21:04 +0000

    Add 3DS to Cocoa version, and enhance 3DS export.

--- LDExporter/LD3dsExporter.h
+++ LDExporter/LD3dsExporter.h
@@ -20,25 +20,25 @@ public:
 	virtual std::string getExtension(void) const { return "3ds"; }
 	virtual ucstring getTypeDescription(void) const;
 protected:
-	typedef std::map<TCULong, int> ULongIntMap;
 	typedef std::map<int, int> IntIntMap;
+	typedef std::map<std::string, int> StringIntMap;
 	~LD3dsExporter(void);
 	void dealloc(void);
 	virtual void initSettings(void) const;
-	void doExport(LDLModel *pModel, const TCFloat *matrix,
-		int colorNumber);
+	void doExport(LDLModel *pModel, Lib3dsMesh *parentMesh,
+		const TCFloat *matrix, int colorNumber);
 	int getMaterial(int colorNumber);
 	void writeShapeLine(Lib3dsMesh *mesh, LDLShapeLine *pShapeLine,
 		const TCFloat *matrix, int colorNumber);
 	void writeTriangle(Lib3dsMesh *mesh, const TCVector *points, int i0,
 		int i1, int i2, int colorNumber, const TCFloat *matrix);
+	std::string getMeshName(LDLModel *model);
 
 	// NOTE: loadSettings is NOT virtual: it's called from the constructor.
 	void loadSettings(void);
 
-	ULongIntMap m_colors;
+	StringIntMap m_names;
 	IntIntMap m_colorNumbers;
-	int m_numMeshes;
 	Lib3dsFile *m_file;
 	LDLModel *m_topModel;
 };
