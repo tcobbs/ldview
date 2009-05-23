3619bf5787b02e47d2f99f75e27f764fc8f91c17
Peter Bartfai <pbartfai@stardust.hu>
2009-05-23 13:30:51 +0000

    delete unused parts

--- QT/qt4wrapper.h
+++ QT/qt4wrapper.h
@@ -4,19 +4,11 @@
 #include <q3filedialog.h>
 #define QPopupMenu	QMenu
 #include <q3popupmenu.h>
-//#define QListBox	Q3ListBox
-//#define QListBoxItem Q3ListBoxItem
-//#define QListBoxText Q3ListBoxText
-//#include <q3listbox.h>
-//#define QListView	Q3ListView
-//#define QListViewItem	Q3ListViewItem
-#include <q3listview.h>
 #define QProgressBar	Q3ProgressBar
 #include <q3progressbar.h>
 #include <q3dragobject.h>
 #define QRangeControl	QSpinBox
 #define QButton		Q3Button
-#include <q3button.h>
 #define QMimeSourceFactory	Q3MimeSourceFactory
 #include <Q3MimeSourceFactory>
 #define QImageDrag	Q3ImageDrag
@@ -25,8 +17,6 @@
 #include <q3header.h>
 #include <QMenuItem>
 #define  getimage(x)    QPixmap(qPixmapFromMimeSource(x))
-#include <Q3CheckListItem>
-//#define QCheckListItem  Q3CheckListItem
 #include <Q3ScrollView>
 #define QScrollView Q3ScrollView
 #include <Q3HBox>
