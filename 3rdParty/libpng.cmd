b2ac1b97edec48bb59e8f65caa00f0e0a61c65a3
Peter Bartfai <pbartfai@stardust.hu>
2011-02-24 15:33:00 +0000

    libpng 1.5.1

--- 3rdParty/libpng.cmd
+++ 3rdParty/libpng.cmd
@@ -1,1 +1,4 @@
-nmake -f scripts\makefile.vcawin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib" clean all
+rem libpng 1.5.1
+nmake -f scripts\makefile.vcwin32 clean
+copy /y scripts\pnglibconf.h.prebuilt pnglibconf.h
+nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib123" all
