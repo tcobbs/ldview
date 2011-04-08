1b0ea7497fce431305f82455b645b34dedc129f2
Peter Bartfai <pbartfai@stardust.hu>
2011-04-08 12:36:23 +0000

    bugfix

--- QT/thumbnailtest.sh
+++ QT/thumbnailtest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./LDView LDView ../8464.mpd -SaveSnapshot=/tmp/8464.png \
+./LDView ../8464.mpd -SaveSnapshot=/tmp/8464.png \
 -SaveWidth=128 -SaveHeight=128 -ShowErrors=0 -SaveActualSize=0
-
+ls -l /tmp/8464.png
