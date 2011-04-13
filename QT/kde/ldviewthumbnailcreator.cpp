037b1d320ab09800bd69bcc422f071b1a9f9e2f1
Peter Bartfai <pbartfai@stardust.hu>
2011-04-13 09:43:04 +0000

    update

--- QT/kde/ldviewthumbnailcreator.cpp
+++ QT/kde/ldviewthumbnailcreator.cpp
@@ -10,37 +10,31 @@ class LDViewCreator : public ThumbCreator
 {
 public:
 	LDViewCreator() {QFile file(QString("/tmp/ldviewkde.log"));
-        if(file.open(IO_WriteOnly|IO_Append))
+        if(file.open(QIODevice::WriteOnly|QIODevice::Append))
         {
                 QTextStream str(&file);
                 str << "started\n";
                 file.close();
 	}};
 	virtual bool create (const QString &path, int, int, QImage &img);
-	virtual Flags flags() { return (Flags)(DrawFrame | BlendIcon);}
+	virtual Flags flags() const { return (Flags)(DrawFrame | BlendIcon);}
 };
 
 bool LDViewCreator::create (const QString &path, int w, int h, QImage &img)
 {
 	QFile file(QString("/tmp/ldviewkde.log"));
-	if(file.open(IO_WriteOnly|IO_Append))
+	if(file.open(QIODevice::WriteOnly|QIODevice::Append))
 	{
 		QTextStream str(&file);
 		str << path <<"\n";
 		file.close();
 	}
-	QProcess *process = new QProcess(QString("/usr/local/bin/LDView"));
-	process->addArgument(path);
-	process->addArgument(QString("-SaveSnapshot=/tmp/ldview-kde.png"));
-	process->addArgument(QString("-SaveWidth=")+QString::number(w));
-	process->addArgument(QString("-SaveHeight=")+QString::number(h));
-	process->addArgument(QString("-ShowErrors=0"));
-	process->addArgument(QString("-SaveActualSize=0"));
-	process->start();
-	while(process->isRunning())
-	{
-		usleep(50000);
-	}
+	QProcess *process = new QProcess;
+	process->start("/usr/local/bin/LDView", QStringList() << path << "-SaveSnapshot=/tmp/ldview-kde.png" << 
+											QString("-SaveWidth=")+QString::number(w) <<
+											QString("-SaveHeight=")+QString::number(h) <<
+											"-ShowErrors=0" << "-SaveActualSize=0");
+	process->waitForFinished();
 	return img.load(QString("/tmp/ldview-kde.png"));
 //	char command[1024];
 //	sprintf(command,"/usr/local/bin/LDView %s -SaveSnapshot=%s -SaveWidth=%i -SaveHeight=%i -ShowErrors=0 -SaveActualSize=0",path.ascii(),"/tmp/ldview-kde.png",w,h);
