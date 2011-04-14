d154842388992d7b873eeb1d88f6689203b2c2bf
Peter Bartfai <pbartfai@example.com>
2011-04-14 13:08:04 +0000

    create uniq temp file and delete it at the end

--- QT/kde/ldviewthumbnailcreator.cpp
+++ QT/kde/ldviewthumbnailcreator.cpp
@@ -1,4 +1,5 @@
 #include "ldviewthumbnailcreator.h"
+#include <QTemporaryFile>
 #include <kdebug.h>
 
 extern "C"
@@ -12,20 +13,32 @@ extern "C"
 
 LDViewCreator::LDViewCreator()
 {
-	kWarning(1111) << "LDView thumbnail plugin started";
 }
 
 bool LDViewCreator::create (const QString &path, int w, int h, QImage &img)
 {
-	kWarning(1111) << "LDView thumbnail CREATE";
+	QString tmpname;
+	bool result;
+	QTemporaryFile tmpfile;
+	if (tmpfile.open())
+	{
+		tmpname=tmpfile.fileName();
+		tmpfile.close();
+		tmpfile.remove();
+	}
+	tmpname+=".png";
+//	kWarning(1111) << "LDView thumbnail CREATE" << path << w << h << tmpname;
 	QProcess *process = new QProcess;
-	process->start("/usr/local/bin/LDView", QStringList() << path << "-SaveSnapshot=/tmp/ldview-kde.png" << 
-											QString("-SaveWidth=")+QString::number(w) <<
-											QString("-SaveHeight=")+QString::number(h) <<
-											"-ShowErrors=0" << "-SaveActualSize=0");
+	process->start("/usr/local/bin/LDView", 
+		QStringList() << path << "-SaveSnapshot="+tmpname << 
+		QString("-SaveWidth=")+QString::number(w) <<
+		QString("-SaveHeight=")+QString::number(h) <<
+		"-ShowErrors=0" << "-SaveActualSize=0");
 	if (!process->waitForFinished())
 		return false;
-	return img.load(QString("/tmp/ldview-kde.png"));
+	result = img.load(tmpname);
+	QFile::remove (tmpname);
+	return result;
 }
 
 ThumbCreator::Flags LDViewCreator::flags() const
