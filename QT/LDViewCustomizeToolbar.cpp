23f0302be3c6d1158d826a14ced7c6bb07628bdc
Peter Bartfai <pbartfai@example.com>
2011-04-20 12:24:45 +0000

    initial work on toolbar customization

---
+++ QT/LDViewCustomizeToolbar.cpp
@@ -0,0 +1,67 @@
+#include <TCFoundation/TCStringArray.h>
+#include <TCFoundation/TCUserDefaults.h>
+#include <LDLib/LDUserDefaultsKeys.h>
+#include <TCFoundation/mystring.h>
+
+#include "ModelViewerWidget.h"
+#include "LDViewCustomizeToolbar.h"
+
+CustomizeToolbar::CustomizeToolbar(QWidget *parent, ModelViewerWidget *modelWidget)
+	:QDialog(parent),CustomizeToolbarPanel(),
+	modelWidget(modelWidget)
+{
+	setupUi(this);
+    connect( addButton, SIGNAL( clicked() ), this, SLOT( doAdd() ) );
+    connect( removeButton, SIGNAL( clicked() ), this, SLOT( doRemove() ) );
+    connect( resetButton, SIGNAL( clicked() ), this, SLOT( doReset() ) );
+    connect( closeButton, SIGNAL( clicked() ), this, SLOT( doClose() ) );
+    connect( moveUpButton, SIGNAL( clicked() ), this, SLOT( doMoveUp() ) );
+    connect( moveDownButton, SIGNAL( clicked() ), this, SLOT( doMoveDown() ) );
+    connect( CurrentButtonListView, SIGNAL( currentItemChanged ( QListWidgetItem * , QListWidgetItem * ) ), this, SLOT( doCurrentButtonSelected(QListWidgetItem *,QListWidgetItem *) ) );
+	connect( AvailableButtonListView, SIGNAL( currentItemChanged ( QListWidgetItem * , QListWidgetItem * ) ), this, SLOT( doAvailableButtonSelected(QListWidgetItem *,QListWidgetItem *) ) );
+
+    modelViewer = modelWidget->getModelViewer();
+}
+
+CustomizeToolbar::~CustomizeToolbar(void)
+{
+}
+
+void CustomizeToolbar::show(void)
+{
+	QDialog::show();
+	raise();
+}
+
+void CustomizeToolbar::doAdd(void)
+{
+}
+
+void CustomizeToolbar::doRemove(void)
+{
+}
+
+void CustomizeToolbar::doClose(void)
+{
+}
+
+void CustomizeToolbar::doReset(void)
+{
+}
+
+void CustomizeToolbar::doMoveUp(void)
+{
+}
+
+void CustomizeToolbar::doMoveDown(void)
+{
+}
+
+void CustomizeToolbar::doCurrentButtonSelected(QListWidgetItem *current,QListWidgetItem *previous)
+{
+}
+
+void CustomizeToolbar::doAvailableButtonSelected(QListWidgetItem *current,QListWidgetItem *previous)
+{
+}
+
---
+++ QT/LDViewCustomizeToolbar.h
@@ -0,0 +1,38 @@
+#ifndef __LDVIEWCUSTOMIZETOOLBAR_H__
+#define __LDVIEWCUSTOMIZETOOLBAR_H__
+
+#include <TCFoundation/TCTypedObjectArray.h>
+#include "ui_CustomizeToolbar.h"
+#include <QDialog>
+
+class ModelViewerWidget;
+class LDrawModelViewer ;
+class TCStringArray;
+
+class CustomizeToolbar : public QDialog , Ui::CustomizeToolbarPanel
+{
+	Q_OBJECT
+public:
+	CustomizeToolbar(QWidget *parent,ModelViewerWidget *modelWidget);
+	~CustomizeToolbar(void);
+
+	void show(void);
+	void clear(void);
+
+public slots:
+	void doAdd(void);
+	void doRemove(void);
+	void doClose(void);
+	void doReset(void);
+	void doMoveUp(void);
+	void doMoveDown(void);
+	void doCurrentButtonSelected(QListWidgetItem *,QListWidgetItem *);
+	void doAvailableButtonSelected(QListWidgetItem *,QListWidgetItem *);
+
+protected:
+
+	LDrawModelViewer *modelViewer;
+	ModelViewerWidget *modelWidget;
+};
+
+#endif 
