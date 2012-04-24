9d1a6e75197b3bce0677b4dc91fdb8776fe9983d
Peter Bartfai <pbartfai@example.com>
2012-04-24 14:43:12 +0000

    debian related update

--- 3rdParty/vbox-ldview.cmd
+++ 3rdParty/vbox-ldview.cmd
@@ -6,8 +6,8 @@ set ZEROIZE=0
 set USER=root
 set PASSWORD=root123
 
-set ENGINE=virtualbox
-rem set ENGINE=vmware
+rem set ENGINE=virtualbox
+set ENGINE=vmware
 
 rem    --------------------------
 rem    No Changes below this line
@@ -16,10 +16,15 @@ rem    --------------------------
 set EXEC=--username %USER% --password %PASSWORD% --verbose  --wait-stderr
 set VBM=%PROGRAMFILES%\Oracle\VirtualBox\VBoxManage.exe
 
-set VMRUN=%PROGRAMFILES%\VMWare\VMWare Workstation\vmrun.exe
+IF EXIST "%PROGRAMFILES%\VMWare\VMWare Workstation\vmrun.exe" SET VMRUN=%PROGRAMFILES%\VMWare\VMWare Workstation\vmrun.exe
+IF EXIST "%PROGRAMFILES(X86)%\VMWare\VMWare Workstation\vmrun.exe" SET VMRUN=%PROGRAMFILES(X86)%\VMWare\VMWare Workstation\vmrun.exe
+
 set OPT=-T ws -gu %USER% -gp %PASSWORD%
 
+
 if not exist rpm mkdir rpm
+
+if not exist deb mkdir deb
 
 if "x%1x" == "xx" (
 	shift
@@ -79,11 +84,14 @@ IF "%ENGINE%"=="vmware" (
 )
 echo Checking/Installing developement packages
 call :RUN "if [ -f /etc/redhat-release -a ! -f /root/ldview-dependency-installed ] ; then for pkg in cvs make qt-devel gcc gcc-c++ rpm-build boost-devel mesa-libOSMesa-devel kdebase-devel libpng-devel libjpeg-turbo-devel tinyxml-devel ; do if ! rpm -q --quiet $pkg ; then yum install -y $pkg ; fi; done ; touch /root/ldview-dependency-installed ; fi"
+call :RUN "if [ -f /etc/debian_version -a ! -f /root/ldview-dependency-installed ] ; then for pkg in gcc cvs make g++ libqt4-dev libboost-thread-dev libpng-dev libjpeg-dev libtinyxml-dev cmake kdelibs5-dev fakeroot lintian ; do dpkg-query -W --showformat='${Package;-30}\t${Status}\n' $pkg|grep -qv not-installed;if [ $? -eq 1 ]  ;then apt-get -y install $pkg;fi;done;touch /root/ldview-dependency-installed ; fi"
 echo Checking out CVS repository ...
 call :RUN "if [ -d /root ] ; then cd /root ; fi ; mkdir -p lego;cd lego;cvs -q -z3 -d:pserver:anonymous@ldview.cvs.sourceforge.net/cvsroot/ldview co LDView;cd LDView/QT;if [ -x /usr/lib/qt4/bin/qmake ] ; then /usr/lib/qt4/bin/qmake ; else qmake ; fi ; true make all"
 echo Building the packages ...
 call :RUN "if [ -d /root ] ; then export HOME=/root ; fi ; if [ -f /etc/redhat-release ] ; then rm -f /root/rpmbuild/RPMS/*/*.rpm ; cd /root/lego/LDView/QT ; rpmbuild -bb LDView.spec; if [ -d /mnt/hgfs/lego ] ; then DST=/mnt/hgfs/lego ; else mount -t vboxsf lego /mnt ; DST=/mnt ; fi ; cp -f /root/rpmbuild/RPMS/*/*.rpm $DST/rpm ; fi"
-rem call :RUN  "if [ -d /root ] ; then export HOME=/root ; fi ; if [ -f /etc/debian_version ] ; then cd /root/lego/LDView/QT ; ./makedeb ; fi"
+if "%ENGINE%"=="vmware" (
+call :RUN  "if [ -d /root ] ; then export HOME=/root ; fi ; if [ -f /etc/debian_version ] ; then cd /root/lego/LDView/QT ; ./makedeb ; if [ -d /mnt/hgfs/lego ] ; then DST=/mnt/hgfs/lego ; else mount -t vboxsf lego /mnt ; DST=/mnt ; fi ; cp -f ldview*.deb $DST/deb ; fi"
+)
 echo Updating Linux ...
 call :RUN "if [ -f /etc/redhat-release ] ; then yum -y -x 'kernel*' update ; fi"
 call :RUN "if [ -f /etc/debian_version ] ; then apt-get -y upgrade ; fi"
