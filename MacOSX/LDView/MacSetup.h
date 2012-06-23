35f9d461704edc26888503502409032150cf6a39
Travis Cobbs <tcobbs@gmail.com>
2012-06-23 23:08:28 +0000

    Add common header for Mac-specific settings.

---
+++ MacOSX/LDView/MacSetup.h
@@ -0,0 +1,18 @@
+/*
+ *  MacSetup.h
+ *  LDView
+ *
+ *  Created by Travis Cobbs on 6/11/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import <Cocoa/Cocoa.h>
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED <= 1040
+
+typedef int32_t NSInteger;
+typedef float CGFloat;
+
+#endif // Tiger
+
