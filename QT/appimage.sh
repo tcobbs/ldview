481a481d100833b0efecf731329f6379ab914450
Peter Bartfai <pbartfai@stardust.hu>
2017-02-27 09:33:43 +0100

    AppImage creation update

--- QT/appimage.sh
+++ QT/appimage.sh
@@ -3,4 +3,7 @@ mkdir -p AppImage
 wget -c https://github.com/probonopd/linuxdeployqt/releases/download/3/linuxdeployqt-3-x86_64.AppImage
 chmod a+x linuxdeployqt*.AppImage
 make INSTALL_ROOT=AppImage install
+cp -f desktop/ldview.desktop AppImage/
+cp -f images/LDViewIcon.png AppImage/gnome-ldraw.png
 ./linuxdeployqt*.AppImage AppImage/usr/bin/LDView -bundle-non-qt-libs
+./linuxdeployqt*.AppImage AppImage/usr/bin/LDView -appimage
