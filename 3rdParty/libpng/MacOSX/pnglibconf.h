0b5713544e02a9addf37444d74746899c74d385f
Travis Cobbs <tcobbs@gmail.com>
2017-03-12 15:00:53 -0700

    Update to latest libpng, and update Mac version to used 3rd-party library binaries checked into source tree.

--- 3rdParty/libpng/MacOSX/pnglibconf.h
+++ 3rdParty/libpng/MacOSX/pnglibconf.h
@@ -1,6 +1,6 @@
 /* pnglibconf.h - library build configuration */
 
-/* libpng version 1.6.23, June 9, 2016 */
+/* libpng version 1.6.28, January 5, 2017 */
 
 /* Copyright (c) 1998-2016 Glenn Randers-Pehrson */
 
@@ -203,7 +203,7 @@
 #define PNG_USER_HEIGHT_MAX 1000000
 #define PNG_USER_WIDTH_MAX 1000000
 #define PNG_ZBUF_SIZE 8192
-#define PNG_ZLIB_VERNUM 0x1250
+#define PNG_ZLIB_VERNUM 0x1280
 #define PNG_Z_DEFAULT_COMPRESSION (-1)
 #define PNG_Z_DEFAULT_NOFILTER_STRATEGY 0
 #define PNG_Z_DEFAULT_STRATEGY 1
