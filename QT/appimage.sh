4529a61f880c84a5d9d85ee938df8ffc3769b5a0
Peter Bartfai <pbartfai@stardust.hu>
2017-07-13 09:56:00 +0200

    AppImage version update

--- QT/appimage.sh
+++ QT/appimage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p AppImage
-wget -c https://github.com/probonopd/linuxdeployqt/releases/download/4/linuxdeployqt-4-x86_64.AppImage
+wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 chmod a+x linuxdeployqt*.AppImage
 make INSTALL_ROOT=AppImage install
 cp -f desktop/ldview.desktop AppImage/
