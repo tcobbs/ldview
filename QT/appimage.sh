931505c59df8e2f466d039bfa855caf69b8aa0ca
Peter Bartfai <pbartfai@stardust.hu>
2017-11-15 15:33:27 +0100

    AppImage enahancement

--- QT/appimage.sh
+++ QT/appimage.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+test -d AppImage || rm -rf AppImage
 mkdir -p AppImage
-wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+if [ ! -x linuxdeployqt-continuous-x86_64.AppImage ] ; then
+wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
 chmod a+x linuxdeployqt*.AppImage
+fi
 make INSTALL_ROOT=AppImage install
 cp -f desktop/ldview.desktop AppImage/
 cp -f images/LDViewIcon.png AppImage/gnome-ldraw.png
 ./linuxdeployqt*.AppImage AppImage/usr/bin/LDView -bundle-non-qt-libs
 ./linuxdeployqt*.AppImage AppImage/usr/bin/LDView -appimage
+test -d AppImage || rm -rf AppImage
