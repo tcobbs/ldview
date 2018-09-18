8e19a650a4bb74de26f35c1f33183d438fd4c11a
Peter Bartfai <pbartfai@stardust.hu>
2018-09-18 11:47:26 +0200

    build language dll fils

--- build.cmd
+++ build.cmd
@@ -1,2 +1,17 @@
 @echo off
 msbuild /p:Configuration=Release LDView.vcxproj
+cd Translations\Hungarian
+msbuild /p:Configuration=Release Hungarian.vcxproj
+cd ..\..
+
+cd Translations\German
+msbuild /p:Configuration=Release German.vcxproj
+cd ..\..
+
+cd Translations\Italian
+msbuild /p:Configuration=Release Italian.vcxproj
+cd ..\..
+
+cd Translations\Czech
+msbuild /p:Configuration=Release Czech.vcxproj
+cd ..\..
