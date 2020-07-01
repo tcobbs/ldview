45305d3c223d85f14901f96bb7b761e0e0c4d5de
Peter Bartfai <pbartfai@stardust.hu>
2020-07-01 21:51:19 +0200

    Windows Build batch update

--- build.cmd
+++ build.cmd
@@ -1,17 +1,15 @@
 @echo off
-msbuild /p:Configuration=Release LDView.vcxproj
-cd Translations\Hungarian
-msbuild /p:Configuration=Release Hungarian.vcxproj
-cd ..\..
+set VS2019=/p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
+set VS2015=/p:PlatformToolset=v140 /p:WindowsTargetPlatformVersion=8.1
+set VS=%VS2015%
+set CONFIG=/p:Configuration=Release
+msbuild %CONFIG% %VS% LDView.sln
+msbuild %CONFIG% %VS% LDView.sln /t:Launcher /p:Platform=win32
+msbuild %CONFIG% %VS% Translations\Hungarian\Hungarian.vcxproj
+msbuild %CONFIG% %VS% Translations\German\German.vcxproj
+msbuild %CONFIG% %VS% Translations\Italian\Italian.vcxproj
+msbuild %CONFIG% %VS% Translations\Czech\Czech.vcxproj
+msbuild %CONFIG% %VS% LDViewThumbs\LDViewThumbs.vcxproj /p:SolutionDir=%CD%\ /p:Platform=win32
+msbuild %CONFIG% %VS% LDViewThumbs\LDViewThumbs.vcxproj /p:SolutionDir=%CD%\
 
-cd Translations\German
-msbuild /p:Configuration=Release German.vcxproj
-cd ..\..
-
-cd Translations\Italian
-msbuild /p:Configuration=Release Italian.vcxproj
-cd ..\..
-
-cd Translations\Czech
-msbuild /p:Configuration=Release Czech.vcxproj
-cd ..\..
+if exist "c:\Program Files (x86)\Inno Setup 6\iscc.exe" "c:\Program Files (x86)\Inno Setup 6\iscc.exe" LDView64.iss
