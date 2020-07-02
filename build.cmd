999eba2265183554b40797274e80b578798bb569
Peter Bartfai <pbartfai@stardust.hu>
2020-07-02 09:47:52 +0200

    Windows build batch enhancement

--- build.cmd
+++ build.cmd
@@ -1,14 +1,14 @@
 @echo off
 set VS2019=/p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 set VS2015=/p:PlatformToolset=v140 /p:WindowsTargetPlatformVersion=8.1
-set VS=%VS2015%
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat" set VS=%VS2019%
 set CONFIG=/p:Configuration=Release
-msbuild %CONFIG% %VS% LDView.sln
+msbuild %CONFIG% %VS% LDView.sln /p:Platform=x64
 msbuild %CONFIG% %VS% LDView.sln /t:Launcher /p:Platform=win32
-msbuild %CONFIG% %VS% Translations\Hungarian\Hungarian.vcxproj
-msbuild %CONFIG% %VS% Translations\German\German.vcxproj
-msbuild %CONFIG% %VS% Translations\Italian\Italian.vcxproj
-msbuild %CONFIG% %VS% Translations\Czech\Czech.vcxproj
+msbuild %CONFIG% %VS% Translations\Hungarian\Hungarian.vcxproj /p:Platform=x64
+msbuild %CONFIG% %VS% Translations\German\German.vcxproj       /p:Platform=x64
+msbuild %CONFIG% %VS% Translations\Italian\Italian.vcxproj     /p:Platform=x64
+msbuild %CONFIG% %VS% Translations\Czech\Czech.vcxproj         /p:Platform=x64
 msbuild %CONFIG% %VS% LDViewThumbs\LDViewThumbs.vcxproj /p:SolutionDir=%CD%\ /p:Platform=win32
 msbuild %CONFIG% %VS% LDViewThumbs\LDViewThumbs.vcxproj /p:SolutionDir=%CD%\
 
