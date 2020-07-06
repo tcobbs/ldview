7d56c7ebb9d63bf4453e7fe76306b413c62e30af
Peter Bartfai <pbartfai@stardust.hu>
2020-07-06 09:39:23 +0200

    Windows build batch: add Inno Setup 5.x and Microsoft c++ Build Tools

--- build.cmd
+++ build.cmd
@@ -2,6 +2,7 @@
 set VS2019=/p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 set VS2015=/p:PlatformToolset=v140 /p:WindowsTargetPlatformVersion=8.1
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat" set VS=%VS2019%
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsx86_amd64.bat" set VS=%VS2019%
 set CONFIG=/p:Configuration=Release
 msbuild %CONFIG% %VS% LDView.sln /p:Platform=x64
 msbuild %CONFIG% %VS% LDView.sln /t:Launcher /p:Platform=win32
@@ -12,4 +13,11 @@ msbuild %CONFIG% %VS% Translations\Czech\Czech.vcxproj         /p:Platform=x64
 msbuild %CONFIG% %VS% LDViewThumbs\LDViewThumbs.vcxproj /p:SolutionDir=%CD%\ /p:Platform=win32
 msbuild %CONFIG% %VS% LDViewThumbs\LDViewThumbs.vcxproj /p:SolutionDir=%CD%\ /p:Platform=x64
 
-if exist "C:\Program Files (x86)\Inno Setup 6\iscc.exe" "c:\Program Files (x86)\Inno Setup 6\iscc.exe" LDView64.iss
+if not exist "C:\Program Files (x86)\Inno Setup 6\iscc.exe" goto noinno6
+"c:\Program Files (x86)\Inno Setup 6\iscc.exe" LDView64.iss
+goto end
+:noinno6
+if not exist "C:\Program Files (x86)\Inno Setup 5\iscc.exe" goto end
+"c:\Program Files (x86)\Inno Setup 5\iscc.exe" LDView64.iss
+
+:end
