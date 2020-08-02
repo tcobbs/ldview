8bef88c5b8b4b69ae97bd439fc2660653305ce3a
Travis Cobbs <tcobbs@gmail.com>
2020-08-02 14:57:11 -0700

    4.4 Beta 1 for Mac setup.

--- MacOSX/LDView/builddmg.sh
+++ MacOSX/LDView/builddmg.sh
@@ -3,11 +3,15 @@ if [ -z "${1}" ]; then
 	echo "Usage: builddmg.sh [directory]"
 	exit 1
 fi
-if [ ! -d "${1}/Contents" ]; then
+if [ -d "${1}/Contents" ]; then
+    app_root="${1}"
+elif [ -d "${1}/Products/Users/${USER}/Applications/LDView.app/Contents" ]; then
+    app_root="${1}/Products/Users/${USER}/Applications/LDView.app"
+else
 	echo Invalid directory: ${1}
 	exit 1
 fi
-echo Stripping executable...
+echo Creating Disk Image from ${app_root}...
 if [ ! -d distrib ]; then
 	mkdir distrib
 fi
@@ -15,13 +19,13 @@ if [ -d distrib/LDView ]; then
 	rm -rf distrib/LDView
 fi
 mkdir distrib/LDView
-FileVersion=`plutil -p "${1}/Contents/Info.plist" | grep CFBundleShortVersionString | cut -d= -f2 | cut -d\" -f2 | sed "s/ /_/g"`
+FileVersion=`plutil -p "${app_root}/Contents/Info.plist" | grep CFBundleShortVersionString | cut -d= -f2 | cut -d\" -f2 | sed "s/ /_/g"`
 #FileVersion=`grep 'VALUE "FileVersion"' ../../LDView.rc | cut -d, -f2 | cut -d\" -f2 | cut -d\\\\ -f1 | sed "s/ /_/g"`
 Filename="LDView_${FileVersion}"
 echo Copying files...
 cp ../../8464.mpd distrib/LDView/
 cp ../../ChangeHistory.html distrib/LDView
-cp -R "$1" distrib/LDView/
+cp -R "${app_root}" distrib/LDView/
 cp ../../license.txt distrib/LDView
 cp ../../m6459.ldr distrib/LDView
 echo Building DMG...
