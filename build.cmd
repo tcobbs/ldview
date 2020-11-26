73be4abf0baf48e0b4ee520cb3afc505899ce832
Peter Bartfai <pbartfai@stardust.hu>
2020-11-26 17:10:55 +0100

    Windows build update

--- build.cmd
+++ build.cmd
@@ -3,6 +3,7 @@ set VS2019=/p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 set VS2015=/p:PlatformToolset=v140 /p:WindowsTargetPlatformVersion=8.1
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat" set VS=%VS2019%
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsx86_amd64.bat" set VS=%VS2019%
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsx86_amd64.bat" set VS=%VS2019%
 set CONFIG=/p:Configuration=Release
 msbuild %CONFIG% %VS% LDView.sln /p:Platform=x64
 msbuild %CONFIG% %VS% LDView.sln /t:Launcher /p:Platform=win32
