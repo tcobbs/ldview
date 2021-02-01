4014987cb6df3eb0f4c3daa14c74c67f1d692f7c
Peter Bartfai <pbartfai@stardust.hu>
2021-02-01 17:27:06 +0100

    Docker: Alpine linux added to os-report

--- QT/docker/os-report.sh
+++ QT/docker/os-report.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo OS,Qt5,gcc,gl2ps,tinyxml,zlib,libpng,libjpeg,OSMesa
-for os in archlinux/base centos:8 fedora:32 mageia:7 opensuse/leap debian:10 ubuntu:20.04 ; do
+for os in archlinux/base centos:8 fedora:33 mageia:7 opensuse/leap debian:10 ubuntu:20.04 alpine:latest; do
 	linux=`echo $os| cut -f1 -d:`
 	linux=`echo $linux|cut -f1 -d/`
 	echo -n $os,
--- QT/docker/query-package.sh
+++ QT/docker/query-package.sh
@@ -24,7 +24,7 @@ elif [ -f /etc/debian_version ] ; then
 	echo " "
 elif [ -f /etc/mandriva-release -o -f /etc/mageia-release ] ; then
 	for pkg in lib64qt5base5-devel gcc lib64gl2ps-devel lib64tinyxml-devel lib64zlib1 lib64png-devel lib64jpeg-devel lib64osmesa-devel ; do
-		echo -n `urpmq -f $pkg 2>/dev/null|sed "s/\.$ARCH//g"|sed 's/\.mga[0-9]*//g'`,
+		echo -n `urpmq -f $pkg 2>/dev/null|sed "s/\.$ARCH//g"|sed 's/\.mga[0-9]*//g'| sed 's/|/\n/g'|sort|tail -1`,
 	done
 	echo -n -e '\b'
 	echo " "
@@ -45,6 +45,13 @@ elif grep -q -e openSUSE /etc/os-release ; then
 		zypper -q info $pkg >/tmp/pkginfo.$$
 		echo -n $pkg-`grep ^Version /tmp/pkginfo.$$|cut -f2 -d:|sed 's/ //g'`,
 		rm -f /tmp/pkginfo.$$
+	done
+	echo -n -e '\b'
+	echo " "
+elif [ -f /etc/alpine-release ] ; then
+	apk update -q
+	for pkg in qt5-qtbase-dev gcc gl2ps tinyxml-dev zlib libpng libjpeg-turbo mesa-osmesa ; do
+		echo -n `apk list -q $pkg | cut -f1 -d\ `,
 	done
 	echo -n -e '\b'
 	echo " "
