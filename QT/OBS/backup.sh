27d864324e85df8ddf9f5f31dcc8f7d16f5d023d
Peter Bartfai <pbartfai@stardust.hu>
2021-02-18 14:00:34 +0100

    OBS backup script added

---
+++ QT/OBS/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+osc meta prj home:pbartfai > home.meta
+osc meta prjconf home:pbartfai > home.prjconf
+osc meta pkg home:pbartfai/LDView > LDView.meta
+osc meta pkg home:pbartfai/LDView-qt5 > LDView-qt5.meta
