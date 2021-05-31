83626a02ad171cbd887f3406f25a589fef9d61b3
Peter Bartfai <pbartfai@stardust.hu>
2021-05-31 17:38:58 +0200

    libjpeg: windows build batch update

--- 3rdParty/libjpeg.cmd
+++ 3rdParty/libjpeg.cmd
@@ -1,2 +1,12 @@
-copy /y jconfig.vc jconfig.h
-nmake -f makefile.vc clean libjpeg.lib nodebug=1
+@echo off
+
+if exist jpegsr9d.zip goto srcfound
+curl -OJL https://www.ijg.org/files/jpegsr9d.zip
+:srcfound
+if exist jpeg-9d goto dirfound
+PowerShell Expand-Archive -Path jpegsr9d.zip  -DestinationPath .
+:dirfound
+cd jpeg-9d
+nmake -f Makefile.vs setup-v16
+msbuild jpeg.vcxproj /p:Platform=win32 /p:Configuration=Release
+msbuild jpeg.vcxproj /p:Platform=x64 /p:Configuration=Release
