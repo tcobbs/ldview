c64f1ebb40d8c303c18ae3eb841f90177a98a600
Peter Bartfai <pbartfai@stardust.hu>
2021-05-31 16:18:29 +0200

    libpng build batch update

--- 3rdParty/libpng.cmd
+++ 3rdParty/libpng.cmd
@@ -1,4 +1,22 @@
-rem libpng 1.5.1
+@echo off
+rem libpng 1.6.37
+if exist lpng1637.zip goto srcfound
+curl -OJL https://download.sourceforge.net/libpng/lpng1637.zip
+:srcfound
+
+if exist lpng1637 goto dirfound
+PowerShell Expand-Archive -Path lpng1637.zip  -DestinationPath .
+:dirfound
+
+
+
+if exist zlib1211.zip goto zipfound
+curl -OJL https://zlib.net/zlib1211.zip
+:zipfound
+if exist zlib1211 goto zipdirfound
+PowerShell Expand-Archive -Path zlib1211.zip  -DestinationPath .
+:zipdirfound
+cd lpng1637
 nmake -f scripts\makefile.vcwin32 clean
 copy /y scripts\pnglibconf.h.prebuilt pnglibconf.h
-nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib123" all
+nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib-1.2.11" all
