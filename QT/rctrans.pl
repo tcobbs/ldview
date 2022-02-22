ef84ff5e5a6ad8dffc5e7126961051ff7d931836
Peter Bartfai <pbartfai@stardust.hu>
2022-02-22 17:49:21 +0100

    RC translation script update

--- QT/rctrans.pl
+++ QT/rctrans.pl
@@ -38,9 +38,10 @@ sub load {
 }
 
 sub dumptrans {
-    ($filename,$from,$to) = @_;
+    ($filename,$from,$to,$outfile) = @_;
 	$fromtext=$from."text";
     open(FILE,$filename);
+	open(OFILE,'>'.$outfile) || die "open error";
     while($sor=<FILE>)
 	{
 		$sor =~s/code_page\([0-9]*\)/code_page\(1250\)/;
@@ -60,12 +61,22 @@ sub dumptrans {
 			}
 		}
 		$sor=~ s/\"Icons/\"..\\\\..\\\\Icons/  ;
-		print $sor;
+		$sor=~ s/\"Textures/\"..\/..\/Textures/ ;
+		$sor=~ s/^\#include \"/\#include \"..\/..\// ;
+		print OFILE $sor;
     }
     close FILE;
+	close OFILE;
 }
 
 load("../LDView.rc","english");
 load("../Translations/Hungarian/LDView.rc","hungarian");
+load("../Translations/German/LDView.rc","german");
+load("../Translations/Italian/LDView.rc","italian");
+load("../Translations/Czech/LDView.rc","czech");
 
-dumptrans("../LDView.rc","english","hungarian");
+dumptrans("../LDView.rc","english","hungarian","/tmp/LDView-hu.rc");
+dumptrans("../LDView.rc","english","czech","/tmp/LDView-cz.rc");
+dumptrans("../LDView.rc","english","italian","/tmp/LDView-it.rc");
+dumptrans("../LDView.rc","english","german","/tmp/LDView-de.rc");
+
