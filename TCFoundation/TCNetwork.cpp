fb649ccb0167d0eff944682dc80616cd095d1705
Peter Bartfai <pbartfai@stardust.hu>
2022-12-12 09:55:48 +0100

    Qt: warning fix

--- TCFoundation/TCNetwork.cpp
+++ TCFoundation/TCNetwork.cpp
@@ -38,7 +38,11 @@ TCNetwork::TCNetworkSetupCleanup::~TCNetworkSetupCleanup(void)
 #endif // WIN32
 
 TCNetwork::TCNetwork(void)
+#if defined (_QT) || defined (__APPLE__) || defined(_OSMESA)
+	:dataSocket(0),
+#else
 	:dataSocket(NULL),
+#endif
 	 connected(0),
 	 errorNumber(0),
 	 errorString(NULL)
