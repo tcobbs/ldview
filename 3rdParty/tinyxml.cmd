2b13e083cce44b9b8b934f68d4badba7803237cd
Peter Bartfai <pbartfai@stardust.hu>
2023-04-27 08:34:23 +0200

    tinyxml build batch update

--- 3rdParty/tinyxml.cmd
+++ 3rdParty/tinyxml.cmd
@@ -1,2 +1,10 @@
+@echo off
+if exist tinyxml_2_6_2.zip goto srcfound
+curl -OJL https://sourceforge.net/projects/tinyxml/files/tinyxml/2.6.2/tinyxml_2_6_2.zip/download
+:srcfound
+if exist tinyxml goto dirfound
+PowerShell Expand-Archive -Path tinyxml_2_6_2.zip -DestinationPath .
+:dirfound
+cd tinyxml
 cl /MT /nologo /W3 /GX /O2 /D WIN32 /D NDEBUG /D _MBCS /D _LIB /D TIXML_USE_STL /FR /FD /c tiny*.cpp
 link -lib /nologo /out:tinyxml_STL.lib tiny*.obj
