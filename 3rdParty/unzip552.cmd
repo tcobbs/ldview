9c8a4a804ee014624783fd450487e1e1cdf72447
Peter Bartfai <pbartfai@stardust.hu>
2023-04-27 09:09:30 +0200

    unzip552 build batch update

--- 3rdParty/unzip552.cmd
+++ 3rdParty/unzip552.cmd
@@ -1,1 +1,9 @@
+@echo off
+if exist unzip552.zip goto srcfound
+curl -OJL https://sourceforge.net/projects/infozip/files/UnZip%205.x%20and%20earlier/5.52/unzip552.zip/download
+:srcfound
+if exist unzip552 goto dirfound
+PowerShell Expand-Archive -Path unzip552.zip -DestinationPath unzip552
+:dirfound
+cd unzip552
 nmake -f win32\Makefile USEZLIB=1 clean lib LOCAL_UNZIP="-I..\zlib -DUSE_ZLIB" cvarsdll=$(cvarsmt)
