7a00f85a48827bf7ab5849d9263a0976544abab5
Peter Bartfai <pbartfai@stardust.hu>
2023-04-27 08:55:27 +0200

    zlib build batch update

--- 3rdParty/zlib-x64.cmd
+++ 3rdParty/zlib-x64.cmd
@@ -1,1 +1,11 @@
-nmake -f win32/Makefile.msc AS=ml64 OBJA="gvmat64.obj inffas8664.obj inffasx64.obj" CFLAGS="-MT -O2 -nologo -I. -DASMV -DASMINF"  clean zlib.lib
+@echo off
+if exist zlib1213.zip goto zipfound
+curl -OJL https://zlib.net/zlib1213.zip
+
+:zipfound
+if exist zlib-1.2.13 goto zipdirfound
+PowerShell Expand-Archive -Path zlib1213.zip -DestinationPath .
+
+:zipdirfound
+cd zlib-1.2.13
+nmake -f win32/Makefile.msc AS=ml64 CFLAGS="-MT -O2 -nologo -I. -DASMV -DASMINF"  clean zlib.lib
