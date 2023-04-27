d0bfb13e9af9f669f3ac1b83215bb43ba3f02ef8
Peter Bartfai <pbartfai@stardust.hu>
2023-04-27 09:37:02 +0200

    libpng build batch update

--- 3rdParty/libpng.cmd
+++ 3rdParty/libpng.cmd
@@ -1,25 +1,25 @@
 @echo off
-rem libpng 1.6.37
-if exist lpng1637.zip goto srcfound
-curl -OJL https://download.sourceforge.net/libpng/lpng1637.zip
+rem libpng 1.6.39
+if exist lpng1639.zip goto srcfound
+curl -OJL https://download.sourceforge.net/libpng/lpng1639.zip
 
 :srcfound
-if exist lpng1637 goto dirfound
-PowerShell Expand-Archive -Path lpng1637.zip -DestinationPath .
+if exist lpng1639 goto dirfound
+PowerShell Expand-Archive -Path lpng1639.zip -DestinationPath .
 
 :dirfound
-if exist zlib1211.zip goto zipfound
-curl -OJL https://zlib.net/zlib1211.zip
+if exist zlib1213.zip goto zipfound
+curl -OJL https://zlib.net/zlib1213.zip
 
 :zipfound
-if exist zlib-1.2.11 goto zipdirfound
-PowerShell Expand-Archive -Path zlib1211.zip -DestinationPath .
+if exist zlib-1.2.13 goto zipdirfound
+PowerShell Expand-Archive -Path zlib1213.zip -DestinationPath .
 
 :zipdirfound
-cd lpng1637
+cd lpng1639
 
 :build
 nmake -f scripts\makefile.vcwin32 clean
 copy /y scripts\pnglibconf.h.prebuilt pnglibconf.h
-nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib-1.2.11" all
+nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib-1.2.13" all
 rem msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=win32 /p:PlatformToolset=v142
