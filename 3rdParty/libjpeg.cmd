ec57c0e1bcd041af58fbe740e456a86d417fc35d
Peter Bartfai <pbartfai@stardust.hu>
2023-04-27 09:28:31 +0200

    libjpeg build batch fix

--- 3rdParty/libjpeg.cmd
+++ 3rdParty/libjpeg.cmd
@@ -1,5 +1,4 @@
 @echo off
-
 if exist jpegsr9d.zip goto srcfound
 curl -OJL https://www.ijg.org/files/jpegsr9d.zip
 :srcfound
@@ -8,5 +7,5 @@ PowerShell Expand-Archive -Path jpegsr9d.zip  -DestinationPath .
 :dirfound
 cd jpeg-9d
 nmake -f Makefile.vs setup-v16
-msbuild jpeg.vcxproj /p:Platform=win32 /p:Configuration=Release
-msbuild jpeg.vcxproj /p:Platform=x64 /p:Configuration=Release
+msbuild jpeg.vcxproj /p:Platform=win32 /p:Configuration=Release /p:PlatformToolset=v143
+msbuild jpeg.vcxproj /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v143
