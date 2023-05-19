6ea6fc50b748703aa7162377785503b773bb7005
Peter Bartfai <pbartfai@stardust.hu>
2023-05-19 14:24:05 +0200

    TinyXML build update

--- 3rdParty/tinyxml.cmd
+++ 3rdParty/tinyxml.cmd
@@ -8,3 +8,4 @@ PowerShell Expand-Archive -Path tinyxml_2_6_2.zip -DestinationPath .
 cd tinyxml
 cl /MT /nologo /W3 /GX /O2 /D WIN32 /D NDEBUG /D _MBCS /D _LIB /D TIXML_USE_STL /FR /FD /c tiny*.cpp
 link -lib /nologo /out:tinyxml_STL.lib tiny*.obj
+rem msbuild /p:PlatformToolset=v143 /p:Configuration=Release tinyxml.vcxproj
