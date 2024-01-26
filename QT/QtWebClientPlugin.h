585a5dc696ca6d3b5b4a4c5c862260e63c460932
Peter Bartfai <pbartfai@stardust.hu>
2024-01-26 07:15:12 +0100

    Update QtWebClientPlugin.h

--- QT/QtWebClientPlugin.h
+++ QT/QtWebClientPlugin.h
@@ -1,5 +1,5 @@
-#ifndef __WINWEBCLIENTPLUGIN_H__
-#define __WINWEBCLIENTPLUGIN_H__
+#ifndef __QTWEBCLIENTPLUGIN_H__
+#define __QTWEBCLIENTPLUGIN_H__
 
 #include <TCFoundation/TCWebClient.h>
 #include <QNetworkAccessManager>
@@ -13,4 +13,4 @@ private:
 	static QtWebClientPlugin* plugin;
 };
 
-#endif // __WINWEBCLIENTPLUGIN_H__
+#endif // __QTWEBCLIENTPLUGIN_H__
