b2b223a73b409a9670be46a90ff9d3c41ea51131
Peter Bartfai <pbartfai@stardust.hu>
2024-03-26 20:23:21 +0100

    Docker: add minizip to package report

--- QT/docker/os-report.sh
+++ QT/docker/os-report.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo OS,Qt5,gcc,gl2ps,tinyxml,zlib,libpng,libjpeg,OSMesa,hostname,which
+echo OS,Qt5,gcc,gl2ps,tinyxml,zlib,libpng,libjpeg,OSMesa,hostname,which,minizip
 for os in archlinux oraclelinux:9 fedora:latest mageia:8 opensuse/leap debian:12 ubuntu:22.04 alpine:latest; do
 	linux=`echo $os| cut -f1 -d:`
 	linux=`echo $linux|cut -f1 -d/`
