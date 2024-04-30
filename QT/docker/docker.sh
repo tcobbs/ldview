35b59731b947d4eb31ccfea9fbf84e0c841bf54f
Peter Bartfai <pbartfai@stardust.hu>
2024-04-30 14:52:23 +0200

    Docker fix

--- QT/docker/docker.sh
+++ QT/docker/docker.sh
@@ -3,7 +3,7 @@
 LIST=Dockerfile-*
 if [ -n "$1" ] ; then LIST=$1 ; fi
 for f in $LIST ; do
-	name=`echo $f|sed 's/Dockerfile-//'`
+	name=`echo $f|sed 's/.*Dockerfile-//'`
 	tag=`echo $name|sed 's/_/:/'`
-	docker build --network host -t ldview-$tag $(test -n "$LDVIEW_BRANCH" && echo --build-arg LDVIEW_BRANCH=$LDVIEW_BRANCH) -f Dockerfile-$name .
+	docker build --network host -t ldview-$tag $(test -n "$LDVIEW_BRANCH" && echo --build-arg LDVIEW_BRANCH=$LDVIEW_BRANCH) -f $f .
 done
