a40d522e3eabc15cf84ecb70cfc2082b04c8eab7
Peter Bartfai <pbartfai@stardust.hu>
2025-09-27 20:10:19 +0200

    Update Windows library build scripts

--- 3rdParty/libjpeg.cmd
+++ 3rdParty/libjpeg.cmd
@@ -1,11 +1,22 @@
 @echo off
-if exist jpegsr9d.zip goto srcfound
-curl -OJL https://www.ijg.org/files/jpegsr9d.zip
+if exist jpegsr9f.zip goto srcfound
+curl -OJL https://www.ijg.org/files/jpegsr9f.zip
 :srcfound
-if exist jpeg-9d goto dirfound
-PowerShell Expand-Archive -Path jpegsr9d.zip  -DestinationPath .
+if exist jpeg-9f goto dirfound
+PowerShell Expand-Archive -Path jpegsr9f.zip  -DestinationPath .
 :dirfound
-cd jpeg-9d
+cd jpeg-9f
 nmake -f Makefile.vs setup-v16
+powershell "(Get-Content jpeg.vcxproj).Replace('<Optimization>Full</Optimization>', """"<Optimization>Full</Optimization>`n<RuntimeLibrary>MultiThreaded</RuntimeLibrary>"""") | Set-Content jpeg.vcxproj"
+ 
 msbuild jpeg.vcxproj /p:Platform=win32 /p:Configuration=Release /p:PlatformToolset=v143
-msbuild jpeg.vcxproj /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v143
+msbuild jpeg.vcxproj /p:Platform=x64   /p:Configuration=Release /p:PlatformToolset=v143
+
+copy jconfig.h   ..\libjpeg\Windows
+copy jconfig.mac ..\libjpeg\MacOSX\jconfig.h
+copy jinclude.h  ..\libjpeg\
+copy jmorecfg.h  ..\libjpeg\
+copy jpeglib.h   ..\libjpeg\
+copy release\win32\jpeg.lib ..\..\lib\libjpeg-vs2022.lib
+copy release\x64\jpeg.lib   ..\..\lib\x64\libjpeg-vs2022.lib
+cd ..
--- 3rdParty/libpng.cmd
+++ 3rdParty/libpng.cmd
@@ -1,25 +1,53 @@
 @echo off
-rem libpng 1.6.39
-if exist lpng1639.zip goto srcfound
-curl -OJL https://download.sourceforge.net/libpng/lpng1639.zip
+rem libpng 1.6.50
+
+if "%1%"  == "uninstall" goto uninstall
+
+if exist lpng1650.zip goto srcfound
+curl -OJL https://download.sourceforge.net/libpng/lpng1650.zip
 
 :srcfound
-if exist lpng1639 goto dirfound
-PowerShell Expand-Archive -Path lpng1639.zip -DestinationPath .
+if exist lpng1650 goto dirfound
+PowerShell Expand-Archive -Path lpng1650.zip -DestinationPath .
 
 :dirfound
-if exist zlib1213.zip goto zipfound
-curl -OJL https://zlib.net/zlib1213.zip
+if exist zlib131.zip goto zipfound
+curl -OJL https://zlib.net/zlib131.zip
 
 :zipfound
-if exist zlib-1.2.13 goto zipdirfound
-PowerShell Expand-Archive -Path zlib1213.zip -DestinationPath .
+if exist zlib-1.3.1 goto zipdirfound
+PowerShell Expand-Archive -Path zlib131.zip -DestinationPath .
 
 :zipdirfound
-cd lpng1639
+cd lpng1650
 
 :build
-nmake -f scripts\makefile.vcwin32 clean
+powershell "(Get-Content projects\vstudio\zlib.props).Replace('..\..\..\..\zlib<', '..\..\..\..\zlib-1.3.1<') | Set-Content projects\vstudio\zlib.props"
+rem nmake -f scripts\makefile.vcwin32 clean
 copy /y scripts\pnglibconf.h.prebuilt pnglibconf.h
-nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib-1.2.13" all
-rem msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=win32 /p:PlatformToolset=v142
+rem nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib-1.3.1" all
+msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=Win32 /p:PlatformToolset=v143 /t:Rebuild
+
+powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('Win32', 'X64') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
+msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=X64 /p:PlatformToolset=v143 /t:Rebuild
+powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('X64', 'Win32') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
+
+if not "%1%"  == "install" goto end
+copy png.h		..\libpng
+copy pngconf.h		..\libpng
+copy pngpriv.h		..\libpng
+copy pnglibconf.h	..\libpng\Windows
+copy "projects\vstudio\Release Library\libpng16.lib" ..\..\lib\libpng16-vs2022.lib
+copy "projects\vstudio\X64\Release Library\libpng16.lib" ..\..\lib\X64\libpng16-vs2022.lib
+goto end
+
+:uninstall
+git checkout -- libpng\png.h
+git checkout -- libpng\pngconf.h
+git checkout -- libpng\pngpriv.h
+git checkout -- libpng\Windows\pnglibconf.h
+git checkout -- ..\lib\libpng16-vs2017.lib
+git checkout -- ..\lib\x64\libpng16-vs2019.lib
+
+:end
+if exist png.h cd ..
