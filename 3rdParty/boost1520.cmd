9690cac0cc3d54c982c57d1521adef862c41fffb
Peter Bartfai <pbartfai@stardust.hu>
2025-09-27 19:51:29 +0200

    Remove boost build scripts

--- 3rdParty/boost1520.cmd
+++
@@ -1,8 +0,0 @@
-cd tools\build\v2\engine
-call build
-cd bin.*
-copy bjam.exe ..\..\..\..\..
-cd ..\..\..\..\..
-bjam --with-date_time --with-thread --with-chrono link=static runtime-link=static variant=release stage release debug
-rem x64
-rem bjam --with-date_time --with-thread --with-chrono architecture=x86 address-model=64 link=static runtime-link=static variant=release stage release debug
