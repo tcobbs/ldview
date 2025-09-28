cb3473f95f157b9a6230aede63a98baa8f600599
Peter Bartfai <pbartfai@stardust.hu>
2025-09-28 20:01:07 +0200

    Windows: lib3ds library build script update

--- 3rdParty/lib3ds.cmd
+++ 3rdParty/lib3ds.cmd
@@ -7,5 +7,10 @@ PowerShell Expand-Archive -Path lib3ds-20080909.zip -DestinationPath .
 :dirfound
 cd lib3ds-20080909
 cd src
-cl /nologo /MT /D WIN32 /D NDEBUG /D _WINDOWS /D LIB3DS_EXPORTS /c *.c
-link /lib /nologo /out:lib3ds.lib *.obj
+"%VCToolsInstallDir%"\bin\HostX64\x86\cl /nologo /MT /D WIN32 /D NDEBUG /D _WINDOWS /D LIB3DS_EXPORTS /c *.c
+link /lib /nologo /MACHINE:X86 /out:lib3ds.lib *.obj
+copy lib3ds.lib ..\..\..\lib\lib3ds-vs2022.lib
+"%VCToolsInstallDir%"\bin\HostX64\x64\cl /nologo /MT /D WIN32 /D NDEBUG /D _WINDOWS /D LIB3DS_EXPORTS /c *.c
+link /lib /nologo /MACHINE:X64 /out:lib3ds.lib *.obj
+copy lib3ds.lib ..\..\..\lib\x64\lib3ds-vs2022.lib
+cd ..\..
