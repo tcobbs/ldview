329550020a3f35bdce01155bef9df99b30a6b406
Peter Bartfai <pbartfai@stardust.hu>
2025-09-29 14:29:54 +0200

    Windows library build script changed to VS2019

--- 3rdParty/libjpeg.cmd
+++ 3rdParty/libjpeg.cmd
@@ -1,22 +1,27 @@
 @echo off
-if exist jpegsr9f.zip goto srcfound
-curl -OJL https://www.ijg.org/files/jpegsr9f.zip
+set JPEGVER=9f
+
+if exist jpegsr%JPEGVER%.zip goto srcfound
+curl -OJL https://www.ijg.org/files/jpegsr%JPEGVER%.zip
 :srcfound
-if exist jpeg-9f goto dirfound
-PowerShell Expand-Archive -Path jpegsr9f.zip  -DestinationPath .
-:dirfound
-cd jpeg-9f
-nmake -f Makefile.vs setup-v16
+if exist jpeg-%JPEGVER% goto dirfound
+PowerShell Expand-Archive -Path jpegsr%JPEGVER%.zip  -DestinationPath .
+cd jpeg-%JPEGVER%
+nmake /nologo -f Makefile.vs setup-v16
 powershell "(Get-Content jpeg.vcxproj).Replace('<Optimization>Full</Optimization>', """"<Optimization>Full</Optimization>`n<RuntimeLibrary>MultiThreaded</RuntimeLibrary>"""") | Set-Content jpeg.vcxproj"
- 
-msbuild jpeg.vcxproj /p:Platform=win32 /p:Configuration=Release /p:PlatformToolset=v143
-msbuild jpeg.vcxproj /p:Platform=x64   /p:Configuration=Release /p:PlatformToolset=v143
+powershell "(Get-Content jpeg.vcxproj).Replace('<WholeProgramOptimization>true</WholeProgramOptimization>', '<WholeProgramOptimization>false</WholeProgramOptimization>') | Set-Content jpeg.vcxproj"
+cd ..
+
+:dirfound
+cd jpeg-%JPEGVER%
+msbuild jpeg.vcxproj /p:Platform=win32 /p:Configuration=Release /p:PlatformToolset=v142
+msbuild jpeg.vcxproj /p:Platform=x64   /p:Configuration=Release /p:PlatformToolset=v142
 
 copy jconfig.h   ..\libjpeg\Windows
 copy jconfig.mac ..\libjpeg\MacOSX\jconfig.h
 copy jinclude.h  ..\libjpeg\
 copy jmorecfg.h  ..\libjpeg\
 copy jpeglib.h   ..\libjpeg\
-copy release\win32\jpeg.lib ..\..\lib\libjpeg-vs2022.lib
-copy release\x64\jpeg.lib   ..\..\lib\x64\libjpeg-vs2022.lib
+copy release\win32\jpeg.lib ..\..\lib\libjpeg-vs2019.lib
+copy release\x64\jpeg.lib   ..\..\lib\x64\libjpeg-vs2019.lib
 cd ..
--- 3rdParty/libpng.cmd
+++ 3rdParty/libpng.cmd
@@ -23,13 +23,18 @@ cd lpng1650
 
 :build
 powershell "(Get-Content projects\vstudio\zlib.props).Replace('..\..\..\..\zlib<', '..\..\..\..\zlib-1.3.1<') | Set-Content projects\vstudio\zlib.props"
+powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('<DebugInformationFormat>ProgramDatabase</DebugInformationFormat>', '<DebugInformationFormat>None</DebugInformationFormat>') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
+powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('<BrowseInformation>true</BrowseInformation>', '<BrowseInformation>false</BrowseInformation>') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
+powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('<GenerateDebugInformation>true</GenerateDebugInformation>', '<GenerateDebugInformation>false</GenerateDebugInformation>') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
+powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('<WholeProgramOptimization>true</WholeProgramOptimization>', '<WholeProgramOptimization>false</WholeProgramOptimization>') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
+
 rem nmake -f scripts\makefile.vcwin32 clean
 copy /y scripts\pnglibconf.h.prebuilt pnglibconf.h
 rem nmake -f scripts\makefile.vcwin32 AR="link -lib" CFLAGS="-nologo -MT -O2 -W3 -I..\zlib-1.3.1" all
-msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=Win32 /p:PlatformToolset=v143 /t:Rebuild
+msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=Win32 /p:PlatformToolset=v142 /t:Rebuild
 
 powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('Win32', 'X64') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
-msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=X64 /p:PlatformToolset=v143 /t:Rebuild
+msbuild projects\vstudio\libpng\libpng.vcxproj "/p:Configuration=Release Library" /p:SolutionDir=../ /p:Platform=X64 /p:PlatformToolset=v142 /t:Rebuild
 powershell "(Get-Content projects\vstudio\libpng\libpng.vcxproj).Replace('X64', 'Win32') | Set-Content projects\vstudio\libpng\libpng.vcxproj"
 
 if not "%1%"  == "install" goto end
@@ -37,8 +42,8 @@ copy png.h		..\libpng
 copy pngconf.h		..\libpng
 copy pngpriv.h		..\libpng
 copy pnglibconf.h	..\libpng\Windows
-copy "projects\vstudio\Release Library\libpng16.lib" ..\..\lib\libpng16-vs2022.lib
-copy "projects\vstudio\X64\Release Library\libpng16.lib" ..\..\lib\X64\libpng16-vs2022.lib
+copy "projects\vstudio\Release Library\libpng16.lib" ..\..\lib\libpng16-vs2019.lib
+copy "projects\vstudio\X64\Release Library\libpng16.lib" ..\..\lib\X64\libpng16-vs2019.lib
 goto end
 
 :uninstall
