c2c457c17792269e317f34beeab305426534bf5e
Peter Bartfai <pbartfai@stardust.hu>
2025-10-08 14:29:40 +0200

    Mac: libjpeg-turbo build script update

--- 3rdParty/BuildInstructions/MacOSX/libjpeg-turbo.sh
+++ 3rdParty/BuildInstructions/MacOSX/libjpeg-turbo.sh
@@ -9,7 +9,7 @@ if [ "$1" = -uninstall ] ; then
 	exit 0
 fi
 test -f libjpeg-turbo-$JPEGVER.dmg || \
-curl -OJL https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/3.1.2/libjpeg-turbo-3.1.2.dmg
+curl -OJL https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/$JPEGVER/libjpeg-turbo-$JPEGVER.dmg
 hdiutil attach -quiet libjpeg-turbo-$JPEGVER.dmg
 pkgutil --expand /Volumes/libjpeg-turbo-$JPEGVER/libjpeg-turbo.pkg /tmp/libjpeg-turbo
 tar xf /tmp/libjpeg-turbo/libjpeg-turbo.pkg/Payload -C /tmp '*.h' '*.a'
