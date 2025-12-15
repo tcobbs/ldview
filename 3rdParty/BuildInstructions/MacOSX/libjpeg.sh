1aae62c2b888298035e452ce2aae6fa192e53a1d
Peter Bartfai <pbartfai@stardust.hu>
2025-12-15 09:13:36 +0100

    Mac: update jpeg and png library build scripts

--- 3rdParty/BuildInstructions/MacOSX/libjpeg.sh
+++ 3rdParty/BuildInstructions/MacOSX/libjpeg.sh
@@ -12,21 +12,13 @@ fi
 
 test -f jpegsrc.v$JPEGVER.tar.gz || curl -OJL https://www.ijg.org/files/jpegsrc.v$JPEGVER.tar.gz
 
-test -d jpeg-$JPEGVER-x64 || (tar zxf jpegsrc.v${JPEGVER}.tar.gz && mv jpeg-${JPEGVER} jpeg-$JPEGVER-x64 )
-test -d jpeg-$JPEGVER-arm64 || (tar zxf jpegsrc.v${JPEGVER}.tar.gz && mv jpeg-${JPEGVER} jpeg-$JPEGVER-arm64 )
+test -d jpeg-$JPEGVER || tar zxf jpegsrc.v${JPEGVER}.tar.gz
 
-cd jpeg-$JPEGVER-x64
-./configure --target x86_64-apple-darwin
-make CFLAGS="-arch x86_64" LDFLAGS="-arch x86_64"
-cd ..
-cd jpeg-$JPEGVER-arm64
-./configure --target arm64-apple-darwin
-make CFLAGS="-arch arm64" LDFLAGS="-arch arm64"
-cd ..
-strip jpeg-$JPEGVER-*/.libs/libjpeg.a
-libtool -o libjpegu.a jpeg-$JPEGVER-arm64/.libs/libjpeg.a jpeg-$JPEGVER-x64/.libs/libjpeg.a
-cp -af libjpegu.a ../../../lib/MacOSX/
-cp -af jpeg-$JPEGVER-x64/jpeglib.h ../../libjpeg/
-cp -af jpeg-$JPEGVER-x64/jmorecfg.h ../../libjpeg/
-cp -af jpeg-$JPEGVER-x64/jinclude.h ../../libjpeg/
-cp -af jpeg-$JPEGVER-x64/jconfig.h ../../libjpeg/MacOSX
+cd jpeg-$JPEGVER
+./configure CFLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=10.13" LDFLAGS="-arch x86_64 -arch arm64"
+make
+cp -af .libs/libjpeg.a ../../../../lib/MacOSX/libjpegu.a
+cp -af jpeglib.h ../../../libjpeg/
+cp -af jmorecfg.h ../../../libjpeg/
+cp -af jinclude.h ../../../libjpeg/
+cp -af jconfig.h ../../../libjpeg/MacOSX
--- 3rdParty/BuildInstructions/MacOSX/libpng.sh
+++ 3rdParty/BuildInstructions/MacOSX/libpng.sh
@@ -12,20 +12,12 @@ fi
 
 test -f libpng-$PNGVER.tar.gz || curl -OJL https://download.sourceforge.net/libpng/libpng-$PNGVER.tar.gz
 
-test -d libpng-$PNGVER-x64 || (tar zxf libpng-$PNGVER.tar.gz && mv libpng-$PNGVER libpng-$PNGVER-x64 )
-test -d libpng-$PNGVER-arm64 || (tar zxf libpng-$PNGVER.tar.gz && mv libpng-$PNGVER libpng-$PNGVER-arm64 )
-cd libpng-$PNGVER-x64
-test -f config.log || ./configure --target x86_64-apple-darwin
-make CFLAGS="-arch x86_64" LDFLAGS="-arch x86_64"
-cd ..
-cd libpng-$PNGVER-arm64
-test -f config.log || ./configure --target arm64-apple-darwin
-make CFLAGS="-arch arm64 -DPNG_ARM_NEON_OPT=0" LDFLAGS="-arch arm64"
-cd ..
-strip libpng-$PNGVER-*/.libs/libpng16.a
-libtool -o libpng16u.a libpng-$PNGVER-arm64/.libs/libpng16.a libpng-$PNGVER-x64/.libs/libpng16.a
-cp -af libpng16u.a ../../../lib/MacOSX
-cp -af libpng-$PNGVER-x64/png.h \
-       libpng-$PNGVER-x64/pngconf.h \
-       libpng-$PNGVER-x64/pngpriv.h ../../libpng/
-cp -af libpng-$PNGVER-x64/pnglibconf.h ../../libpng/MacOSX
+test -d libpng-$PNGVER || tar zxf libpng-$PNGVER.tar.gz
+cd libpng-$PNGVER
+test -f config.log || ./configure CFLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=10.13" --disable-arm-neon --enable-shared=no LDFLAGS="-arch x86_64 -arch arm64"
+make
+cp -af .libs/libpng16.a ../../../../lib/MacOSX/libpng16u.a
+cp -af png.h \
+       pngconf.h \
+       pngpriv.h ../../../libpng/
+cp -af pnglibconf.h ../../../libpng/MacOSX
