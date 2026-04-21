da63507c9e208de381949ae8b2066f52b3d28052
Peter Bartfai <pbartfai@stardust.hu>
2026-04-21 12:40:52 +0200

    update libpng to 1.6.58

--- 3rdParty/libpng-src.cmd
+++ 3rdParty/libpng-src.cmd
@@ -1,24 +1,25 @@
 @echo off
+set PNGVER=1658
 if "%1%"  == "uninstall" goto uninstall
-if not exist c:\temp\lpng1655.zip curl -OJL --output-dir c:\temp https://download.sourceforge.net/libpng/lpng1655.zip
-if not exist c:\temp\lpng1655 PowerShell Expand-Archive -Path c:\temp\lpng1655.zip -DestinationPath c:\temp
+if not exist c:\temp\lpng%PNGVER%.zip curl -OJL --output-dir c:\temp https://download.sourceforge.net/libpng/lpng%PNGVER%.zip
+if not exist c:\temp\lpng%PNGVER% PowerShell Expand-Archive -Path c:\temp\lpng%PNGVER%.zip -DestinationPath c:\temp
 
-copy /y \temp\lpng1655\*.c       libpng >nul
-copy /y \temp\lpng1655\*.h       libpng >nul
+copy /y \temp\lpng%PNGVER%\*.c       libpng >nul
+copy /y \temp\lpng%PNGVER%\*.h       libpng >nul
 del libpng\pngtest.c
 del libpng\example.c
-copy /y \temp\lpng1655\AUTHORS   libpng >nul
-copy /y \temp\lpng1655\CHANGES   libpng >nul
-copy /y \temp\lpng1655\LICENSE   libpng >nul
-copy /y \temp\lpng1655\README    libpng >nul
-copy /y \temp\lpng1655\projects\vstudio\zlib.props    libpng >nul
-copy /y \temp\lpng1655\projects\vstudio\libpng\libpng.vcxproj    libpng >nul
-copy /y \temp\lpng1655\scripts\pnglibconf.h.prebuilt libpng\pnglibconf.h >nul
-copy /y \temp\lpng1655\scripts\pngwin.rc libpng\ >nul
-copy /y \temp\lpng1655\arm\*.* libpng\arm >nul
+copy /y \temp\lpng%PNGVER%\AUTHORS   libpng >nul
+copy /y \temp\lpng%PNGVER%\CHANGES   libpng >nul
+copy /y \temp\lpng%PNGVER%\LICENSE   libpng >nul
+copy /y \temp\lpng%PNGVER%\README    libpng >nul
+copy /y \temp\lpng%PNGVER%\projects\vstudio\zlib.props    libpng >nul
+copy /y \temp\lpng%PNGVER%\projects\vstudio\libpng\libpng.vcxproj    libpng >nul
+copy /y \temp\lpng%PNGVER%\scripts\pnglibconf.h.prebuilt libpng\pnglibconf.h >nul
+copy /y \temp\lpng%PNGVER%\scripts\pngwin.rc libpng\ >nul
+copy /y \temp\lpng%PNGVER%\arm\*.* libpng\arm >nul
 
 cd ..
-"c:\Program Files\Git\usr\bin\patch.exe" -N -p1 -i 3rdParty\libpng.patch
+"%USERPROFILE%\AppData\Local\Programs\Git\usr\bin\patch.exe" -N -p1 -i 3rdParty\libpng.patch
 for /r %%f in (*.rej) do @del %%f
 for /r %%f in (*.orig) do @del %%f
 cd 3rdparty\libpng
@@ -46,7 +47,7 @@ goto end
 cd  ..
 git checkout -- *
 rem del c:\temp\lpng*.zip
-rem rd /s /q c:\temp\lpng1655
+rem rd /s /q c:\temp\lpng%PNGVER%
 git status
 cd 3rdparty
 :end
