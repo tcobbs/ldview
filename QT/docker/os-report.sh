8ea13eff8be0784fb8b2fc2d2364476c7ee1ee25
Peter Bartfai <pbartfai@stardust.hu>
2026-06-02 14:51:40 +0200

    Docker: package version query update

--- QT/docker/os-report.sh
+++ QT/docker/os-report.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo OS,Qt5,gcc,gl2ps,tinyxml2,zlib,libpng,libjpeg,OSMesa,hostname,which,minizip
+echo OS,Qt6,Qt5,gcc,gl2ps,tinyxml2,zlib,libpng,libjpeg,OSMesa,hostname,which,minizip
 for os in archlinux oraclelinux:10 fedora:latest mageia:latest opensuse/leap debian:latest ubuntu:latest alpine:latest; do
 	linux=`echo $os| cut -f1 -d:`
 	linux=`echo $linux|cut -f1 -d/`
