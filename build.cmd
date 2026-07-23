1e14eadf41b52463644a632c1162ccd863b53fef
Peter Bartfai <pbartfai@stardust.hu>
2026-07-23 09:26:41 +0200

    Windows: build batch add detection of VS 2026 BuildTools

--- build.cmd
+++ build.cmd
@@ -2,12 +2,16 @@
 set VS2022=/p:PlatformToolset=v143 /p:WindowsTargetPlatformVersion=10.0
 set VS2019=/p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 set VS2015=/p:PlatformToolset=v140 /p:WindowsTargetPlatformVersion=8.1
+set VS18=/p:PlatformToolset=v145 /p:WindowsTargetPlatformVersion=10.0
+
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat" set VS=%VS2019%
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsx86_amd64.bat" set VS=%VS2019%
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsx86_amd64.bat" set VS=%VS2019%
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" set VS=%VS2022%
 if exist "C:\Program Files\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" set VS=%VS2022%
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" set VS=%VS2022%
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat" set VS=%VS18%
+
 set CONFIG=/p:Configuration=Release
 msbuild %CONFIG% %VS% LDView.sln /p:Platform=x64 /p:SolutionDir=%CD%\ /m
 msbuild %CONFIG% %VS% Launcher\Launcher.vcxproj /p:SolutionDir=%CD%\ /p:Platform=win32 /m
